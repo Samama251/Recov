0372abdd3b30b1b717f0a31d5a75f621679bbc02
Samama Usman <samop251@gmail.com>
2024-05-14 18:35:19 +0500

    I have set the claim controller

--- recov/server/controllers/claimController.js
+++ recov/server/controllers/claimController.js
@@ -39,30 +39,32 @@ const CreateClaimRequest = asyncHandler(async (req, res) => {
   }
 });
 const deleteClaimRequest = asyncHandler(async (req, res) => {
-    try {
-      console.log("Hello");
-      console.log(req.body.token);
-      if (!req.body.token) {
-        throw new ApiError(401, "Unauthorized request");
-      }
-      const decodedToken = jwt.verify(req.body.token, "abc123");
-      const user = await User.findById(decodedToken?._id);
-      if (!user) {
-        throw new ApiError(401, "Invalid Access Token");
-      }
-      console.log(req.body);
-      const { id } = req.body;
-      if (!id) {
-        throw new ApiError(400, "Please fill all fields");
-      }
-      const claimItem = await claim.findByIdAndDelete(id);
-      if (claimItem) {
-        res.status(201).json({
-          ok: true,
-          _id: claimItem._id,
-        });
-      } else {
-        throw new ApiError(400, "Invalid data");
-      }
+  try {
+    console.log("Hello");
+    console.log(req.body.token);
+    if (!req.body.token) {
+      throw new ApiError(401, "Unauthorized request");
     }
-}
+    const decodedToken = jwt.verify(req.body.token, "abc123");
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+    console.log(req.body);
+    const { id } = req.body;
+    if (!id) {
+      throw new ApiError(400, "Please fill all fields");
+    }
+    const claimItem = await claim.findByIdAndDelete(id);
+    if (claimItem) {
+      res.status(201).json({
+        ok: true,
+        _id: claimItem._id,
+      });
+    } else {
+      throw new ApiError(400, "Invalid data");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
